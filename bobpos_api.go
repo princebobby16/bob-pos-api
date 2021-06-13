e77dfe21e9c5a29bce52fb97860e23d7ed93d255
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2021-06-13 12:28:16 +0000

    new commit

---
+++ bobpos_api.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	_, err := fmt.Fprint(w, "Hello, World!")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
